bffd1e92981c0d798c8a416c79ad332c87ae46df
Daniel Souza Affonso Ferreira <daniel.souza.af@gmail.com>
2018-12-13 09:39:14 -0300

    changing application name

--- config/app.php
+++ config/app.php
@@ -169,11 +169,11 @@ return [
         /*
          * Application Service Providers...
          */
-        App\Providers\AppServiceProvider::class,
-        App\Providers\AuthServiceProvider::class,
-        // App\Providers\BroadcastServiceProvider::class,
-        App\Providers\EventServiceProvider::class,
-        App\Providers\RouteServiceProvider::class,
+        RiaChatBots\Providers\AppServiceProvider::class,
+        RiaChatBots\Providers\AuthServiceProvider::class,
+        // RiaChatBots\Providers\BroadcastServiceProvider::class,
+        RiaChatBots\Providers\EventServiceProvider::class,
+        RiaChatBots\Providers\RouteServiceProvider::class,
 
     ],
 
